84ba0cd3b016352e65962de13ace0ee8527ebf0a
datashoe <xuhao7891@126.com>
2018-01-28 00:25:50 +0800

    编写了elk-kmeans的大部分函数

--- myProject/core/src/com/self/core/clustering/yinyangKmeans.scala
+++ myProject/core/src/com/self/core/clustering/yinyangKmeans.scala
@@ -4,6 +4,7 @@ import org.apache.spark.mllib.clustering.KMeans
 
 
 class yinyangKmeans extends KMeans {
+
 }
 
 
--- myProject/core/src/org/apache/spark/clustering/mllib/ElkKmeans.scala
+++ myProject/core/src/org/apache/spark/clustering/mllib/ElkKmeans.scala
@@ -7,7 +7,7 @@
 //import org.apache.spark.storage.StorageLevel
 //import org.apache.spark.Logging
 //import org.apache.spark.util.random.XORShiftRandom
-//
+//import org.apache.spark.mllib.linalg.BLAS.{axpy, scal}
 //import scala.collection.mutable.{Map => mutableMap}
 //
 //
@@ -75,22 +75,94 @@
 //
 //    // first iteration
 //    // ----compute the center and bound information for each point.
-//    val dataWithBound: RDD[VectorWithBound] = firstIteration(data, initialCenters)
+//    val (dataWithBound, inputCenters) = firstIteration(data, initialCenters)
+//
 //
 //    // Lloyd iteration
-//    Lloyd(dataWithBound)
+//    Lloyd(dataWithBound, inputCenters)
 //
 //
 //  }
 //
 //
 //  /** The Lloyd iteration function. */
-//  private def Lloyd(data: RDD[VectorWithBound])
+//  private def Lloyd(data: RDD[VectorWithBound], inputCenters: KCenters)
 //  : KMeansModel = {
+//    val sc = data.sparkContext
+//    var centers = inputCenters
+//    val centersBC = sc.broadcast(centers)
+//    val minCost = centersBC.value.DisMap.values.min
+//    var i = 0
+//    var flag = true
+//    while (i < maxIterations && flag) {
+//      val iteratorRdd = data.map {
+//        vec => {
+//          // --------------------------------------------
+//          val v = vec.vector
+//          var id = vec.id
+//          val low = vec.low
+//          var upper = vec.up
+//          var r = vec.r
+//
+//          if (upper < minCost) {
+//            val restCenters = Array
+//              .tabulate(k - 1)(i => if (i < id) i else i + 1)
+//            restCenters.foreach {
+//              i => {
+//                val centerI = centersBC.value.centers(i)
+//                if (upper > low(i) && upper > centersBC.value.disCompute(i, id) / 2) {
+//                  var dist2center = upper
+//                  if (r) {
+//                    dist2center = Vectors.sqdist(centersBC.value.centers(id), v)
+//                    low(id) = dist2center
+//                    upper = dist2center
+//                    r = false
+//                  }
+//                  if (dist2center > low(i) || dist2center > centersBC.value.disCompute(i, id) / 2) {
+//                    val dis = Vectors.sqdist(centerI, v)
+//                    if (dist2center < 0.01) { // vectorWithBound need to add a cost.
+//                      id = i
+//                      upper = dis
+//                      low(id) = dis
+//                    }
+//                  }
+//                }
 //
 //
-//    //
+//              }
 //
+//            }
+//
+//          }
+//
+//          VectorWithBound(v, id, low, upper, r)
+//        }
+//      }
+//
+//      val centerArr = iteratorRdd.map(vec => (vec.id, (1, vec.vector))).reduceByKey {
+//        case ((c1, v1), (c2, v2)) =>
+//          axpy(1.0, v1, v2)
+//          (c1 + c2, v2)
+//      }.collect().map {
+//        case (key, (count, sv)) =>
+//          scal(1 / count, sv)
+//          (key, sv)
+//      }.sortBy(_._1).map(_._2)
+//
+//      // 计算新旧centers之间的移动
+//      val move = centerArr.zip(centers.centers).map{case (v1, v2) => Vectors.sqdist(v1, v2)}
+//
+//      // 检验是否达到收敛
+//      flag = !move.forall(mv => mv < epsilon*epsilon)
+//
+//      // 更新upper和low以及r
+//
+//      // 更新中心点
+//      centers = new KCenters(centerArr)
+//    }
+//
+//    // 输出
+//    centers
 //
 //  }
 //
@@ -121,12 +193,33 @@
 //
 //
 //  /** Find closest centers */
-//  private def findClosest(points: Vector): (Int, Double) = {
+//  private def findClosest(points: Vector, Centers: KCenters)
+//  : (Int, Double) = {
+//    val inferCost = Centers.DisMap.values.min
+//    var bestId = 0
+//    var bestCost = Double.PositiveInfinity
+//    var i = 0
+//    var flag = true
 //
+//    while(flag && i < Centers.centers.length){
+//      val cost = Vectors.sqdist(Centers.centers(i), points)
+//      if(cost < bestCost){
+//        bestId = i
+//        bestCost = cost
+//      }
+//      if(bestCost < inferCost / 2)
+//        flag = false
+//      i += 1
+//    }
+//    (bestId, bestCost)
 //  }
 //
 //  /** Find closest centers when we have more information of bound */
-//  private def findClosest(points: VectorWithBound): (Int, Double) = {
+//  private def findClosest(points: VectorWithBound)
+//  : (Int, Double) = {
+//
+//
+//
 //
 //
 //  }
@@ -139,13 +232,31 @@
 //
 //  private def firstIteration(data: RDD[Vector],
 //                             initialCenter: KCenters)
-//  : RDD[VectorWithBound] = {
+//  : (RDD[VectorWithBound], KCenters) = {
+//    // Update data with the closest centers and bound information.
+//    val sc = data.sparkContext
+//    val bcCenters = sc.broadcast(initialCenter)
+//    val dataWithBound = data.map{point =>
+//      val (id, cost) = findClosest(point, bcCenters.value)
+//      val low = Array.tabulate(k)(i => if(i == id) cost else 0.0)
+//      val up = cost
+//      VectorWithBound(point, id, low, up, false)
+//    }
 //
+//    // Update the centers.
+//    val centersArr = dataWithBound.map(v => (v.id, (1, v.vector)))
+//      .reduceByKey{
+//      case ((c1, v1), (c2, v2)) =>
+//        axpy(1.0, v1, v2)
+//        (c1 + c2, v2)
+//    }.collect().map{
+//        case (key, (count, sv)) =>
+//          scal(1 / count, sv)
+//          (key, sv)
+//      }.sortBy(_._1).map(_._2)
 //
-//
-//
-//
-//
+//    val newCenters = new KCenters(centersArr)
+//    (dataWithBound, newCenters)
 //  }
 //
 //
@@ -166,8 +277,8 @@
 //
 //case class VectorWithBound(vector: Vector,
 //                           id: Int,
-//                           l: Array[Vector],
-//                           u: Double,
+//                           low: Array[Double],
+//                           up: Double,
 //                           r: Boolean)
 //
 //class KCenters(val centers: Array[Vector]) extends Serializable {
