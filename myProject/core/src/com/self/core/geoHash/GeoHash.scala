61321ebd4274c8836df242a7523c4d02c58b279d
datashoe <xuhao7891@126.com>
2018-03-30 18:45:53 +0800

    geoHash调优
    Signed-off-by: datashoe <xuhao7891@126.com>

--- myProject/core/src/com/self/core/geoHash/GeoHash.scala
+++ myProject/core/src/com/self/core/geoHash/GeoHash.scala
@@ -12,27 +12,27 @@ class GeoHash(val hashLength: Int = 9) extends Serializable {
   val iterNumsInfo = GeoIterNumsInfo(hashLength * 5 - hashLength / 2 *5, hashLength / 2 *5)
 
   /** Base32编码 */
-  var digits = Array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q',
-    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+  val digits: Array[Char] = Array(
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm',
+    'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z')
 
   /**
-    * 经度初始迭代 --用于编码位数为奇数的情况, 此时经度需要先迭代一次
-    * ----
+    * 经纬度范围
     */
-  private def initial(axis: Double, range: (Double, Double)): (Double, Boolean) = {
-    val mid = (range._1 + range._2) / 2
-    if(axis >= mid) (axis - mid, true) else (mid - axis, false)
-  }
+  val longitudeRange: (Double, Double) = (-180.0, 180.0)
+  val latitudeRange: (Double, Double) = (-90.0, 90.0)
 
-  /** 迭代 */
+  /** 单点编码 */
   def encode(longitude: Double, latitude: Double): String = {
+    var i = 0
     var flag = true // true时对纬度二分, 否则对经度二分, 以经度二分开头
-    var (lgLower: Double, lgUpper: Double) = (-180.0, 180.0)
-    var (laLower: Double, laUpper: Double) = (-90.0, 90.0)
+    var (lgLower: Double, lgUpper: Double) = longitudeRange
+    var (laLower: Double, laUpper: Double) = latitudeRange
     var bitNum = 0
-    val hashString = new Array[Int](hashLength)
-    for(i <- 0 until (hashLength * 5)){
+    var hashString = ""
+    while(i < hashLength * 5){
       if(flag){
         val lgMid: Double = (lgLower + lgUpper) / 2.0
         if(longitude >= lgMid){
@@ -40,6 +40,7 @@ class GeoHash(val hashLength: Int = 9) extends Serializable {
           bitNum = bitNum * 2 + 1
         }else{
           lgUpper = lgMid
+          bitNum = bitNum * 2
         }
       }else{
         val laMid: Double = (laLower + laUpper) / 2
@@ -48,16 +49,59 @@ class GeoHash(val hashLength: Int = 9) extends Serializable {
           bitNum = bitNum * 2 + 1
         }else{
           laUpper = laMid
+          bitNum = bitNum * 2
         }
       }
       if(i % 5 == 4){
-        hashString(i / 5) = digits(bitNum)
+        hashString += digits(bitNum)
         bitNum = 0
       }
       flag = !flag
+      i += 1
     }
-    hashString.mkString("")
+    hashString
   }
+
+
+  /** 九点编码 */
+  def encodeWith9Points(longitude: Double, latitude: Double): String = {
+    var i = 0
+    var flag = true // true时对纬度二分, 否则对经度二分, 以经度二分开头
+    var (lgLower: Double, lgUpper: Double) = longitudeRange
+    var (laLower: Double, laUpper: Double) = latitudeRange
+    var bitNum = 0
+    var hashString = ""
+    while(i < hashLength * 5){
+      if(flag){
+        val lgMid: Double = (lgLower + lgUpper) / 2.0
+        if(longitude >= lgMid){
+          lgLower = lgMid
+          bitNum = bitNum * 2 + 1
+        }else{
+          lgUpper = lgMid
+          bitNum = bitNum * 2
+        }
+      }else{
+        val laMid: Double = (laLower + laUpper) / 2
+        if(latitude >= laMid){
+          laLower = laMid
+          bitNum = bitNum * 2 + 1
+        }else{
+          laUpper = laMid
+          bitNum = bitNum * 2
+        }
+      }
+      if(i % 5 == 4){
+        hashString += digits(bitNum)
+        bitNum = 0
+      }
+      flag = !flag
+      i += 1
+    }
+    hashString
+  }
+
+
 
 
 
--- myProject/core/src/com/self/core/testBaseApp/testBaseAPP.scala
+++ myProject/core/src/com/self/core/testBaseApp/testBaseAPP.scala
@@ -13,22 +13,22 @@ object testBaseAPP extends myAPP{
     val data = Array.fill(100000)((rd.nextDouble()*10, rd.nextDouble()*50 + 50))
     val geo = new GeoHash(8)
     val geo2 = new GeoHashMap(8)
-    geo.encode(0.1, 0.1)
-    geo2.encode(0.1, 0.1)
+    println(geo.encode(0, 0))
+    println(geo2.encode(0.0, 0.0))
 
-    val start_time = System.nanoTime
-    val stay_time1 = System.nanoTime()
-    data.map{case (longitude, latitude) => geo.encode(longitude, latitude)}
-
-    val stay_time2 = System.nanoTime()
-    data.map{case (longitude, latitude) => geo2.encode(longitude, latitude)}
-    val end_time = System.nanoTime()
-
-
-    println(stay_time2 - stay_time1, end_time - stay_time2)
-
-    "152419098"
-    "1498647235"
+//    val start_time = System.nanoTime
+//    val stay_time1 = System.nanoTime()
+//    data.map{case (longitude, latitude) => geo.encode(longitude, latitude)}
+//
+//    val stay_time2 = System.nanoTime()
+//    data.map{case (longitude, latitude) => geo2.encode(longitude, latitude)}
+//    val end_time = System.nanoTime()
+//
+//
+//    println(stay_time2 - stay_time1, end_time - stay_time2)
+//
+//    "152419098"
+//    "1498647235"
     // Test SparkContext.
 //    val rdd = sc.parallelize((0 until 100).toList)
 //    val rdd_sum = rdd.reduce(_ + _)
