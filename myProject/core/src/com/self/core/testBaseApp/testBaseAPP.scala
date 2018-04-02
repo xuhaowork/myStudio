a4636ae022a73723213ca8a7d548e8a04f912739
datashoe <xuhao7891@126.com>
2018-04-02 17:39:44 +0800

    调优了geohash
    Signed-off-by: datashoe <xuhao7891@126.com>

--- myProject/core/src/com/self/core/testBaseApp/testBaseAPP.scala
+++ myProject/core/src/com/self/core/testBaseApp/testBaseAPP.scala
@@ -3,6 +3,8 @@ package com.self.core.testBaseApp
 import com.self.core.baseApp.myAPP
 import src.com.self.core.geoHash.{GeoHash, GeoHashMap}
 
+import scala.collection.mutable.ArrayBuffer
+
 
 /**
   * Created by DataShoe on 2018/1/5.
@@ -11,18 +13,22 @@ object testBaseAPP extends myAPP{
   override def run(): Unit = {
     val rd = new java.util.Random(123L)
     val data = Array.fill(100000)((rd.nextDouble()*10, rd.nextDouble()*50 + 50))
-    val geo = new GeoHash(8)
+    val geo = new GeoHash(4)
     val geo2 = new GeoHashMap(8)
-    println(geo.encode(0, 0))
-    println(geo2.encode(0.0, 0.0))
+    println(geo.encode(-10, -10))
+    println(geo.encodeFor9(-10, -10))
 
-//    val start_time = System.nanoTime
-//    val stay_time1 = System.nanoTime()
-//    data.map{case (longitude, latitude) => geo.encode(longitude, latitude)}
-//
-//    val stay_time2 = System.nanoTime()
-//    data.map{case (longitude, latitude) => geo2.encode(longitude, latitude)}
-//    val end_time = System.nanoTime()
+
+    println(Array('0', 'y', 'z').mkString(""))
+//    println(geo2.encode(0.0, 0.0))
+
+    val start_time = System.nanoTime
+    val stay_time1 = System.nanoTime()
+    data.map{case (longitude, latitude) => geo.encode(longitude, latitude)}
+
+    val stay_time2 = System.nanoTime()
+    data.map{case (longitude, latitude) => geo2.encode(longitude, latitude)}
+    val end_time = System.nanoTime()
 //
 //
 //    println(stay_time2 - stay_time1, end_time - stay_time2)
@@ -33,6 +39,31 @@ object testBaseAPP extends myAPP{
 //    val rdd = sc.parallelize((0 until 100).toList)
 //    val rdd_sum = rdd.reduce(_ + _)
 //    println(rdd_sum)
+
+
+
+
+
+
+
+//    while(i < arr.length){
+//      val plusOne = arr(arr.length - 1 - i) + plus
+//      println(plusOne)
+//      if(plusOne >= (1 << 5)){
+//        arr(arr.length - 1 - i) = plusOne - (1 << 5)
+//        plus = 1
+//      }else{
+//        arr(arr.length - 1 - i) = plusOne
+//        plus = 0
+//      }
+//      i += 1
+//    }
+//
+//    println(1 << 5)
+//    println(arr.mkString(","))
+
+
+
 
 
 
--- myProject/core/src/com/self/core/testBaseApp/testMyApp.scala
+++ myProject/core/src/com/self/core/testBaseApp/testMyApp.scala
@@ -30,34 +30,40 @@ object testMyApp extends myAPP with Serializable {
 //
 //    val u = Array(1.0, 2.0, 3.0, 4.0, 5.0)
 
-    val arr1 = Array.range(0, 5)
-    val arr2 = Array.range(0, 10, 2)
+//    val arr1 = Array.range(0, 5)
+//    val arr2 = Array.range(0, 10, 2)
+//
+//    val u = Array.concat(arr1, arr2)
+//    u.foreach(println)
+//
+//
+//    println("-"*80)
+//    Array.iterate(0, 5)(x => x + 3).foreach(println)
+//
+//    println("-"*80)
+//    val newArr = Array.empty[Int] ++ {for(i <- 0 until 5) yield 3*i}
+//    val newArr2 = ArrayBuffer.empty[Int] ++ {for(i <- 0 until 5) yield 3*i}
+//    newArr2.foreach(println)
+//
+//    println("-"*80)
+//    Array.tabulate(10)(i => Array.fill(i + 1)(0.0)).foreach(x => println(x.length))
+//
+//
+//    println("-"*80)
+//    val multiTable = Array.tabulate(9, 9)((i, j) => f"${j + 1}%2d * ${i + 1}%2d = ${(i + 1)*(j + 1)}%2d")
+//    multiTable.foreach(arr => println(arr.mkString("  ")))
+//
+//    val triMultiTable = Array.tabulate(9)(i => Array.range(1, i + 2).map(j => f"$j%2d * ${i + 1}%2d = ${(i + 1) * j}%2d"))
+//    triMultiTable.foreach(arr => println(arr.mkString("  ")))
+//
+//    println("-"*80)
+//    triMultiTable.foreach(x => println(x.length))
 
-    val u = Array.concat(arr1, arr2)
-    u.foreach(println)
+
+    println(Integer.valueOf("0000000000000000001", 2))
 
 
-    println("-"*80)
-    Array.iterate(0, 5)(x => x + 3).foreach(println)
 
-    println("-"*80)
-    val newArr = Array.empty[Int] ++ {for(i <- 0 until 5) yield 3*i}
-    val newArr2 = ArrayBuffer.empty[Int] ++ {for(i <- 0 until 5) yield 3*i}
-    newArr2.foreach(println)
-
-    println("-"*80)
-    Array.tabulate(10)(i => Array.fill(i + 1)(0.0)).foreach(x => println(x.length))
-
-
-    println("-"*80)
-    val multiTable = Array.tabulate(9, 9)((i, j) => f"${j + 1}%2d * ${i + 1}%2d = ${(i + 1)*(j + 1)}%2d")
-    multiTable.foreach(arr => println(arr.mkString("  ")))
-
-    val triMultiTable = Array.tabulate(9)(i => Array.range(1, i + 2).map(j => f"$j%2d * ${i + 1}%2d = ${(i + 1) * j}%2d"))
-    triMultiTable.foreach(arr => println(arr.mkString("  ")))
-
-    println("-"*80)
-    triMultiTable.foreach(x => println(x.length))
 
   }
 
