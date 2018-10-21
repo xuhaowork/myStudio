5cc8f382cff8b16f79e54c74adaac873e6cf7faa
datashoe <xuhao7891@126.com>
2018-10-21 19:34:42 +0800

    小波变换，以及arima模型的试探和学习
    Signed-off-by: datashoe <xuhao7891@126.com>

--- RWorkPlace/时序数据预测.R
+++ RWorkPlace/时序数据预测.R
@@ -1,5 +1,8 @@
 # 时序数据预测 --时序回归所可能用到的拟合模型或回归模型
 
+# library
+library(dplyr)
+
 # 要分析的数据 data.frame
 data <- mutate(
   read.csv("file:///E:/上海出差/时序数据.csv"),
@@ -36,8 +39,7 @@ mse <- function(dt, fit){
 # loess -- stats::loess
 # ----
 # 训练模型 span是拟合度
-loess_model <- loess(labels ~ time, data = train_data, span = 0.4)
-
+loess_model <- loess(labels ~ 1 + timestamp, data = train_data, span = 0.4)
 train_loess = mse(train_data[, "labels"], loess_model$fitted)
 develop_loess = mse(develop_data[, "labels"], predict(loess_model, develop_data[, "timestamp"]))
 # 参数调试
@@ -94,19 +96,32 @@ plot(train_data[, c("timestamp", "labels")], col = "blue", type = "p", lwd = 2, 
 lines(bestModel$x, bestModel$y, col = "red")
 data.frame(train_data, "predict" = bestModel$y)
 
+# ----
+# arima模型试探
+# ----
+# 查看阶数
+dim(data)
+train_data_ts = ts(log10(data[, "labels"][1 : 240]), start = 1)
+acf <- acf(train_data_ts, lag.max=100, plot=FALSE) # 48阶托尾, 检测ma滞后
+plot(acf)
+pacf <- pacf(train_data_ts, lag.max=100, plot=FALSE) # 2阶震荡, 检测ar滞后
+plot(pacf)
+arima_model <- arima(train_data_ts, c(9, 2, 2)) # p, d, q
+tsdiag(arima_model) # 查看模型拟合效果
+
+# 预测并查看效果
+forecast_model <- predict(arima_model, 23)
+mse(log10(data[, "labels"][241: 263]), forecast_model$pred)
+plot((data[, "timestamp"][1: 263]), log10((data[, "labels"][1: 263])), col = "red")
+points(data[, "timestamp"][241: 263], forecast_model$pred, col = "blue")
 
 
+# 周期分解
+train_data_ts <- ts(train_data[, "labels"], frequency = 48)
+plot(train_data_ts)
+train_data_components <- decompose(train_data_ts)
+plot(train_data_components)
 
 
-require(graphics)
-plot(dist ~ speed, data = cars, main = "data(cars)  &  smoothing splines")
-cars.spl <- with(cars, smooth.spline(speed, dist))
-cars.spl
-## This example has duplicate points, so avoid cv = TRUE
-
-lines(cars.spl, col = "blue")
-ss10 <- smooth.spline(cars[,"speed"], cars[,"dist"], df = 10)
-lines(ss10, lty = 2, col = "red")
-legend(5,120,c(paste("default [C.V.] => df =",round(cars.spl$df,1)),
-               "s( * , df = 10)"), col = c("blue","red"), lty = 1:2,
-       bg = 'bisque')
+# 小波分析
+library(wavelets)
