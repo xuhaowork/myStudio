5d93a81ec3459068bfecf5bd160cb3e9a5773b66
datashoe <xuhao7891@126.com>
2019-03-01 17:27:10 +0800

    图像读写

--- projects/SPARK/repos/spark/SparkMainShortCut/src/main/scala/cn/datashoe/sparkBase/TestSparkAPP.scala
+++ projects/SPARK/repos/spark/SparkMainShortCut/src/main/scala/cn/datashoe/sparkBase/TestSparkAPP.scala
@@ -3,6 +3,9 @@ package cn.datashoe.sparkBase
 object TestSparkAPP extends SparkAPP {
   override def run(): Unit = {
 //    println(sc.makeRDD(Seq.range(0, 100)).reduce(_ + _))
+
+    import org.apache.spark.mllib.clustering.KMeans
+
     sqlc.createDataFrame(Seq.range(0, 100).map(Tuple1.apply)).show()
 
   }
