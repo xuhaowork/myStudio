206dec658bdb75b5960b71a72efee1cbc768d735
datashoe <xuhao7891@126.com>
2019-03-27 17:45:53 +0800

    时间格式转换

--- projects/SPARK/repos/spark/SparkUtils/src/main/scala/cn/datashoe/IOUtils/package.scala
+++ projects/SPARK/repos/spark/SparkUtils/src/main/scala/cn/datashoe/IOUtils/package.scala
@@ -2,12 +2,15 @@ package cn.datashoe
 
 import java.io.File
 import java.nio.charset.Charset
+import java.sql.{Connection, ResultSet, ResultSetMetaData, SQLException}
 
 import org.apache.commons.io.FileUtils
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.sql.datasources.hbase.HBaseTableCatalog
+import org.apache.spark.sql.jdbc.JdbcDialects
+import org.apache.spark.sql.types._
 
 
 package object IOUtils {
@@ -240,6 +243,109 @@ package object IOUtils {
 //      }
 
     }
+
+
+  def getCatalystType(
+                       sqlType: Int,
+                       precision: Int,
+                       scale: Int,
+                       signed: Boolean): DataType = {
+    val answer = sqlType match {
+      case java.sql.Types.ARRAY         => null
+      case java.sql.Types.BIGINT        => if (signed) { LongType } else { DecimalType(20,0) }
+      case java.sql.Types.BINARY        => BinaryType
+      case java.sql.Types.BIT           => BooleanType // @see JdbcDialect for quirks
+      case java.sql.Types.BLOB          => BinaryType
+      case java.sql.Types.BOOLEAN       => BooleanType
+      case java.sql.Types.CHAR          => StringType
+      case java.sql.Types.CLOB          => StringType
+      case java.sql.Types.DATALINK      => null
+      case java.sql.Types.DATE          => DateType
+      case java.sql.Types.DECIMAL
+        if precision != 0 || scale != 0 => DecimalType(precision, scale)
+
+      case java.sql.Types.DECIMAL       => DecimalType.SYSTEM_DEFAULT
+      case java.sql.Types.DISTINCT      => null
+      case java.sql.Types.DOUBLE        => DoubleType
+      case java.sql.Types.FLOAT         => FloatType
+      case java.sql.Types.INTEGER       => if (signed) { IntegerType } else { LongType }
+      case java.sql.Types.JAVA_OBJECT   => null
+      case java.sql.Types.LONGNVARCHAR  => StringType
+      case java.sql.Types.LONGVARBINARY => BinaryType
+      case java.sql.Types.LONGVARCHAR   => StringType
+      case java.sql.Types.NCHAR         => StringType
+      case java.sql.Types.NCLOB         => StringType
+      case java.sql.Types.NULL          => null
+
+      case java.sql.Types.NUMERIC
+        if precision == 0  => DecimalType(38, 0)
+
+      case java.sql.Types.NUMERIC
+        if precision != 0 || scale != 0 => DecimalType(precision, scale) // 还没有和支持的最大最小经度进行比较
+      case java.sql.Types.NUMERIC       => DecimalType.SYSTEM_DEFAULT
+      case java.sql.Types.NVARCHAR      => StringType
+      case java.sql.Types.OTHER         => null
+      case java.sql.Types.REAL          => DoubleType
+      case java.sql.Types.REF           => StringType
+      case java.sql.Types.ROWID         => LongType
+      case java.sql.Types.SMALLINT      => IntegerType
+      case java.sql.Types.SQLXML        => StringType
+      case java.sql.Types.STRUCT        => StringType
+      case java.sql.Types.TIME          => TimestampType
+      case java.sql.Types.TIMESTAMP     => TimestampType
+      case java.sql.Types.TINYINT       => IntegerType
+      case java.sql.Types.VARBINARY     => BinaryType
+      case java.sql.Types.VARCHAR       => StringType
+      case _                            => null
+    }
+    if (answer == null) throw new SQLException("Unsupported type " + sqlType)
+    answer
+  }
+
+
+  def  Struct(url:String,conn:Connection,sql:String) :StructType= {
+    val stm = conn.createStatement()
+    var sql1 = ""
+    var rs: ResultSet = null
+    val dialect = JdbcDialects.get(url)
+    if (sql.contains("where")) {
+      sql1 = sql.replaceAll("where.+", "where 1=0")
+    }
+    else if (sql.contains("limit")) {
+      sql1 = sql.replaceAll("limit.+", "where 1=0")
+    }
+    else {
+      sql1 = sql + " where 1=0"
+    }
+
+
+    rs = stm.executeQuery(sql1)
+    val rsmd = rs.getMetaData
+    val ncols = rsmd.getColumnCount
+    val fields = new Array[StructField](ncols)
+    var i = 0
+    while (i < ncols) {
+      val columnName = rsmd.getColumnLabel(i + 1)
+      val dataType = rsmd.getColumnType(i + 1)
+      val typeName = rsmd.getColumnTypeName(i + 1)
+      val fieldSize = rsmd.getPrecision(i + 1)
+      val fieldScale = rsmd.getScale(i + 1)
+      val isSigned = rsmd.isSigned(i + 1)
+      val nullable = rsmd.isNullable(i + 1) != ResultSetMetaData.columnNoNulls
+      val metadata = new MetadataBuilder().putString("name", columnName)
+      metadata.putLong("scale",fieldScale)
+      val columnType =
+        dialect.getCatalystType(dataType, typeName, fieldSize, metadata).getOrElse(
+          getCatalystType(dataType, fieldSize, fieldScale, isSigned))
+      fields(i) = StructField(columnName, columnType, nullable, metadata.build())
+      i = i + 1
+    }
+    rs.close()
+    stm.close()
+    conn.close()
+    new StructType(fields)
+  }
+
 
 
   }
